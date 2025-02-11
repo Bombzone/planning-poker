915a312e11095e1a5784c02597a886066e7f204d
Ryan Posey <ryan.m.posey@gmail.com>
2025-02-11 16:11:47 -0700

    Learning

---
+++ src/header.jsx
@@ -0,0 +1,10 @@
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>Agile Scrum Manager</h1>
+            </div>
+        )
+    }
+}
+export default Header;
